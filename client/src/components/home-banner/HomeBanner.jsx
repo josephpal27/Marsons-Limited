c6078e8bd7044bdbd09be83c66400c72ab5efe73
Joseph Pal <josephpal27@gmail.com>
2026-02-02 17:08:29 +0530

    Home Banner added

---
+++ client/src/components/home-banner/HomeBanner.jsx
@@ -0,0 +1,13 @@
+import './HomeBanner.css';
+
+const HomeBanner = () => {
+    return (
+        <>
+            <div className="home-banner">
+                <video src="/videos/home-banner-1.webm"></video>
+            </div>
+        </>
+    )
+}
+
+export default HomeBanner
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import { Mousewheel, Keyboard } from "swiper/modules";
 import { useEffect, useState } from "react";
 
 import "swiper/css";
+import '../css/Home.css';
+import HomeBanner from "../components/home-banner/HomeBanner";
 
 const Home = () => {
     const [isMobile, setIsMobile] = useState(false);
@@ -18,58 +20,48 @@ const Home = () => {
     if (isMobile) {
         return (
             <>
-                <section className="section">Section 1</section>
+                <section className="section">
+                    <HomeBanner />
+                </section>
                 <section className="section">Section 2</section>
-                <section className="section">Section 3</section>
-                <section className="section">Section 4</section>
-                <section className="section">Section 5</section>
             </>
         );
     }
 
     // Desktop - Vertical Slider
     return (
-        <Swiper
-            direction="vertical"
-            slidesPerView={1}
-            speed={900}
-            modules={[Mousewheel, Keyboard]}
-            style={{ height: "100vh" }}
-            mousewheel={{
-                forceToAxis: true,
-                sensitivity: 0.4,      // prevents fast momentum
-                thresholdDelta: 60,    // ignores tiny gestures
-                thresholdTime: 500,    // blocks rapid repeats
-                releaseOnEdges: false,
-            }}
+        <>
+            <Swiper
+                direction="vertical"
+                slidesPerView={1}
+                speed={900}
+                modules={[Mousewheel, Keyboard]}
+                style={{ height: "100vh" }}
+                mousewheel={{
+                    forceToAxis: true,
+                    sensitivity: 0.4,      // prevents fast momentum
+                    thresholdDelta: 60,    // ignores tiny gestures
+                    thresholdTime: 500,    // blocks rapid repeats
+                    releaseOnEdges: false,
+                }}
 
-            keyboard={{
-                enabled: true,
-                onlyInViewport: true,
-            }}
+                keyboard={{
+                    enabled: true,
+                    onlyInViewport: true,
+                }}
 
-            preventInteractionOnTransition={true}
-        >
-            <SwiperSlide>
-                <section className="section">Section 1</section>
-            </SwiperSlide>
+                preventInteractionOnTransition={true}
+            >
+                <SwiperSlide>
+                    <HomeBanner />
+                </SwiperSlide>
 
-            <SwiperSlide>
-                <section className="section">Section 2</section>
-            </SwiperSlide>
+                <SwiperSlide>
+                    <section className="section">Section 2</section>
+                </SwiperSlide>
 
-            <SwiperSlide>
-                <section className="section">Section 3</section>
-            </SwiperSlide>
-
-            <SwiperSlide>
-                <section className="section">Section 4</section>
-            </SwiperSlide>
-
-            <SwiperSlide>
-                <section className="section">Section 5</section>
-            </SwiperSlide>
-        </Swiper>
+            </Swiper>
+        </>
     );
 };
 
