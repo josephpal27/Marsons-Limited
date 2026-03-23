baf802aed173945ac666e3e0b1530c615e70df3f
Joseph Pal <josephpal27@gmail.com>
2026-03-23 18:23:33 +0530

    Our Clients Updated

--- client/src/components/OurClients.jsx
+++ client/src/components/OurClients.jsx
@@ -4,24 +4,25 @@ const OurClients = () => {
     return (
         <>
             <section className="
-                px-[7%]
-                py-[2rem] sm:py-[2rem] lg:py-[1rem] xl:py-[1.5rem] 2xl:py-[2rem]
+                px-[1rem] sm:px-[5%] lg:px-[7%]
+                py-[0] sm:py-[2rem] lg:py-[1rem] xl:py-[1.5rem] 2xl:py-[2rem]
+                mt-[3rem] sm:mt-0
             ">
                 {/* Head */}
                 <div className="
                     flex flex-col items-center
                 ">
                     <h5 className="
-                        text-[3.5rem] sm:text-[3rem] lg:text-[2.9rem] xl:text-[3.2rem] 2xl:text-[3.5rem]
+                        text-[1.8rem] sm:text-[3rem] lg:text-[2.9rem] xl:text-[3.2rem] 2xl:text-[3.5rem]
                         font-[700] leading-[1.1] text-center
                     ">
                         OUR <br /> <span className="text-[#e9202a]">CLIENTS</span>
                     </h5>
                     <p className="
-                        text-[1.1rem] sm:text-[1.1rem] lg:text-[0.9rem] xl:text-[1rem] 2xl:text-[1.1rem]
+                        text-[1rem] sm:text-[1.1rem] lg:text-[0.9rem] xl:text-[1rem] 2xl:text-[1.1rem]
                         text-[#000] font-[500] text-center
                         mt-[1rem] sm:mt-[1rem] lg:mt-[0.8rem] xl:mt-[0.9rem] 2xl:mt-[1rem]
-                        w-[60%]
+                        sm:w-[60%]
                     ">
                         Trusted by industry leaders worldwide, we deliver reliable power solutions to state utilities, infrastructure projects, and multinational corporations.
                     </p>
@@ -30,7 +31,7 @@ const OurClients = () => {
                 {/* Row */}
                 <div className="
                     mt-[0.5rem]
-                    py-[2rem] sm:py-[2rem] lg:py-[1.6rem] xl:py-[1.8rem] 2xl:py-[2rem]
+                    py-[0] sm:py-[2rem] lg:py-[1.6rem] xl:py-[1.8rem] 2xl:py-[2rem]
                  ">
                     <OurClientLogos />
                 </div>
