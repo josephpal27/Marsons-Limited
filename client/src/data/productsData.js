4359413167e179dabcc0b5755069d2b18d89086b
Joseph Pal <josephpal27@gmail.com>
2026-03-26 17:51:34 +0530

    Products Page Tablet View Updated

--- client/src/data/productsData.js
+++ client/src/data/productsData.js
@@ -30,29 +30,41 @@ export const productsData = [
     {
         id: 2,
         title: "Power Transformers",
-        desc: "Coming Soon",
+        desc: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
         transformerImages: [
             {
                 image: "/images/transformers/power/1.avif",
-                desc: "Marsons manufactures distribution transformers up to 33 kV in single and three phase variants. Options include free breathing, conservator-fitted, and hermetically sealed designs for diverse applications.",
+                desc: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
             },
             {
                 image: "/images/transformers/power/2.avif",
-                desc: "Hermetically sealed transformers are maintenance-free and ideal for harsh outdoor conditions like moisture, salt, and dust, ensuring durability and reliable performance in challenging environments.",
+                desc: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
             },
             {
                 image: "/images/transformers/power/3.avif",
-                desc: "These transformers are widely used in chemical plants and oil and gas terminals, where difficult accessibility makes routine maintenance impractical and dependable operation is critical.",
-            },
-            {
-                image: "/images/transformers/power/4.avif",
-                desc: "Transformers with synthetic coolants are suitable for indoor use with proper ventilation or near load centers where mineral oil may not meet environmental or safety considerations.",
-            },
-            {
-                image: "/images/transformers/power/5.avif",
-                desc: "Range up to 2500 kVA, 33 kV class, 50/60 Hz frequency, compliant with IS, IEC, ANSI, IEEE standards, PCB-free oil, Class A insulation, tap changers, and BIS-approved testing.",
+                desc: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
             },
         ],
         slug: "power-transformers",
+    },
+    {
+        id: 3,
+        title: "Furnace Transformers",
+        desc: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        transformerImages: [
+            {
+                image: "/images/transformers/furnace/1.avif",
+                desc: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+            },
+            {
+                image: "/images/transformers/furnace/2.avif",
+                desc: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+            },
+            {
+                image: "/images/transformers/furnace/3.avif",
+                desc: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+            },
+        ],
+        slug: "furnace-transformers",
     },
 ]
