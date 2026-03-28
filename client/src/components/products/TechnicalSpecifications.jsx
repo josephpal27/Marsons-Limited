062272b6f7868d74b6a51fb042537cb56d941667
Joseph Pal <josephpal27@gmail.com>
2026-03-28 15:05:25 +0530

    Specs Updated

--- client/src/components/products/TechnicalSpecifications.jsx
+++ client/src/components/products/TechnicalSpecifications.jsx
@@ -11,7 +11,6 @@ const TechnicalSpecifications = ({specs}) => {
                     text-[#e9202a] font-[600] text-center
                 ">
                     TECHNICAL SPECIFICATIONS
-                    
                 </h3>
 
                 {/* Table */}
